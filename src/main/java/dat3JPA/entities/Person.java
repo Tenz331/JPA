f3ee2181bd5b949f040b11e3c8f24d76ca723939
Tenz331 <61831296+Tenz331@users.noreply.github.com>
2021-03-02 14:48:08 +0100

    add address entity

--- src/main/java/dat3JPA/entities/Person.java
+++ src/main/java/dat3JPA/entities/Person.java
@@ -6,10 +6,12 @@
 package dat3JPA.entities;
 
 import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 /**
  *
@@ -24,6 +26,8 @@ public class Person implements Serializable {
     private Long p_id;
     private String name;
     private int year;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    private Address address;
 
     public Person() {
     }
@@ -31,6 +35,17 @@ public class Person implements Serializable {
     public Person(String name, int year) {
         this.name = name;
         this.year = year;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+        if(address != null){
+            address.setPerson(this);
+        }
     }
 
     public void setP_id(Long p_id) {
