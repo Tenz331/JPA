bd190145e0a7c1e1c06d3f03bdf93296b33c8b74
Tenz331 <61831296+Tenz331@users.noreply.github.com>
2021-03-02 14:12:35 +0100

    First commit

---
+++ src/main/java/dat3JPA/entities/Tester.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dat3JPA.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author melo-
+ */
+public class Tester {
+    
+    public static void main(String[] args) {
+        
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+    EntityManager em = emf.createEntityManager();
+    
+    Person p1 = new Person("jønke", 1963);
+    Person p2 = new Person("Blondie",1959);
+    
+    
+    em.getTransaction().begin();
+    em.persist(p1);
+    em.persist(p2);
+    
+    em.getTransaction().commit();
+    
+    System.out.println("p1: " + p1.getP_id()+ ", " + p1.getName());
+    System.out.println("p1: " + p2.getP_id()+ ", " + p2.getName());
+    }
+}
---
+++ target/generated-sources/annotations/dat3JPA/entities/Person_.java
@@ -0,0 +1,15 @@
+package dat3JPA.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-02T14:06:41")
+@StaticMetamodel(Person.class)
+public class Person_ { 
+
+    public static volatile SingularAttribute<Person, Integer> year;
+    public static volatile SingularAttribute<Person, String> name;
+    public static volatile SingularAttribute<Person, Long> p_id;
+
+}
