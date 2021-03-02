277c0fc12a32774af61735480cbd69d88fc08d78
Tenz331 <61831296+Tenz331@users.noreply.github.com>
2021-03-02 16:03:38 +0100

    add fee entity

--- src/main/java/dat3JPA/entities/Tester.java
+++ src/main/java/dat3JPA/entities/Tester.java
@@ -5,9 +5,11 @@
  */
 package dat3JPA.entities;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -22,16 +24,35 @@ public class Tester {
     
     Person p1 = new Person("jønke", 1963);
     Person p2 = new Person("Blondie",1959);
+    Person p3 = new Person("Søren",1993);
+    Person p4 = new Person("mathias",1996);
+    
     
     Address a1 = new Address("Store Torv 1", 2323, "Nr.Snede");
     Address a2 = new Address("Langgade 34", 1212, "Valby");
+    Address a3 = new Address("Fredenborgvej 10", 3434, "Frederiksund");
+    Address a4 = new Address("Jyllingevej 2", 4344, "Jyllinge");
     
     p1.setAddress(a1);
     p2.setAddress(a2);
     
+    Fee f1 = new Fee(100);
+    Fee f2 = new Fee(200);
+     Fee f3 = new Fee(300);
+     Fee f4 = new Fee(400);
+    
+    p1.AddFees(f1);
+    p1.AddFees(f3);
+    p2.AddFees(f2);
+    p4.AddFees(f1);
+    p3.AddFees(f4);
+    
+    
     em.getTransaction().begin();
     em.persist(p1);
     em.persist(p2);
+    em.persist(p3);
+    em.persist(p4);
     em.getTransaction().commit();
     
     System.out.println("p1: " + p1.getP_id()+ ", " + p1.getName());
@@ -40,7 +61,18 @@ public class Tester {
     
     System.out.println("Jønkes gade:  " + p1.getAddress().getStreet());
         
-        System.out.println("Lad os se om to-vejs virker: " + a1.getPerson().getName());    
+        System.out.println("Lad os se om to-vejs virker: " + a1.getPerson().getName()); 
+        
+        System.out.println("Hvem har betalt f2? Det har: " + f2.getPerson().getName());
+        
+        System.out.println("Hvad har Jønke betalt: ");
+       
+        TypedQuery<Fee> q1 = em.createQuery("SELECT f FROM Fee f", Fee.class);
+        List<Fee> fees = q1.getResultList();
+        
+        for(Fee f: fees){
+            System.out.println(f.getPerson().getName() + ": " + f.getAmount() + " Kr. Den: " +f.getPayDate() +"Add: " + f.getPerson().getAddress().getCity());
+        }
     
     }
 }
